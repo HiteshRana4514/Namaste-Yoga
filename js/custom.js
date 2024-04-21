2261bce05ba4693b8aa23c45f76653717163ede9
Hitesh <hiteshrana4514@gmail.com>
2024-04-21 21:20:54 +0530

    appointment-form-popup

--- js/custom.js
+++ js/custom.js
@@ -1,9 +1,26 @@
 let mobMenu = document.querySelector(".mob-menu");
+let bodyTag = document.querySelector("body");
 let navigationMenu = document.querySelector(".navigation");
+let numberBtn = document.querySelector(".number-btn");
+let appointmentForm = document.querySelector(".appointment-form");
+let inactiveBtn = document.querySelector(".inactive-btn");
+let formBg = document.querySelector(".form-bg");
 mobMenu.onclick = mobMenuActive;
 function mobMenuActive(){
     this.classList.toggle("active");
     navigationMenu.classList.toggle("active");
+}
+numberBtn.onclick = appointmentFormActive;
+function appointmentFormActive(){
+    appointmentForm.classList.add("active");
+    bodyTag.style.overflow = "hidden";
+    inactiveBtn.onclick = inactiveForm;
+    formBg.onclick = inactiveForm;
+}
+function inactiveForm(){
+    appointmentForm.classList.remove("active");
+    bodyTag.style.removeProperty("overflow"); 
+    // bodyTag.style.overflow = "visible";
 }
 $('.banner-slider').slick({
     dots: false,
