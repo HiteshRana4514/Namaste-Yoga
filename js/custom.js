f11142871a628631bc51595596e9f37f9b401657
Hitesh <hiteshrana4514@gmail.com>
2024-05-18 16:22:57 +0530

    update-banner

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,20 @@
 let mobMenu = document.querySelector(".mob-menu");
 let bodyTag = document.querySelector("body");
 let navigationMenu = document.querySelector(".navigation");
-let numberBtn = document.querySelector(".number-btn");
+let numberBtn = document.querySelectorAll(".number-btn");
 let appointmentForm = document.querySelector(".appointment-form");
 let inactiveBtn = document.querySelector(".inactive-btn");
 let formBg = document.querySelector(".form-bg");
+let menuItem = document.querySelectorAll(".menu-item");
 mobMenu.onclick = mobMenuActive;
 function mobMenuActive(){
     this.classList.toggle("active");
     navigationMenu.classList.toggle("active");
+    bodyTag.classList.toggle("active");
 }
-numberBtn.onclick = appointmentFormActive;
+numberBtn.forEach(function(e){
+    e.onclick = appointmentFormActive;
+})
 function appointmentFormActive(){
     appointmentForm.classList.add("active");
     bodyTag.style.overflow = "hidden";
@@ -23,11 +27,24 @@ function inactiveForm(){
     // bodyTag.style.overflow = "visible";
 }
 $('.banner-slider').slick({
-    dots: false,
+    dots: true,
     infinite: true,
     speed: 300,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
+    autoplaySpeed: 7000,
   });
+menuItem.forEach(function(e,i){
+    e.onclick = function activeItem(){
+        for(let j=0;j<menuItem.length;j++){
+            if(j==i){
+                e.classList.add("active");
+            }
+            else{
+                menuItem[j].classList.remove("active");
+            }
+        }
+        
+    }
+})
