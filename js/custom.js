0f38c3a839533d73496f39d81fa58665f96c06f5
Hitesh <hiteshrana4514@gmail.com>
2024-06-01 19:35:16 +0530

    services-update

--- js/custom.js
+++ js/custom.js
@@ -29,7 +29,7 @@ function inactiveForm(){
 $('.banner-slider').slick({
     dots: true,
     infinite: true,
-    speed: 300,
+    speed: 400,
     slidesToShow: 1,
     slidesToScroll: 1,
     autoplay: true,
@@ -48,3 +48,41 @@ menuItem.forEach(function(e,i){
         
     }
 })
+$('.classes-slider').slick({
+    dots: true,
+    infinite: false,
+    speed: 400,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+    //   {
+    //     breakpoint: 600,
+    //     settings: {
+    //       slidesToShow: 2,
+    //       slidesToScroll: 1
+    //     }
+    //   },
+      {
+        breakpoint: 767,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+                  
